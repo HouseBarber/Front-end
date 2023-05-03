4da1e38de626fede826e6faffe98753c37b967aa
JoileJr <joilejunior2@gmail.com>
2023-05-03 10:58:43 -0300

    alteração na tela de login, e criação da tela de cadastro + funcionalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,21 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { HomeComponent } from './pages/home/home.component';
+
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatMenuModule} from "@angular/material/menu";
-import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 import {RouterOutlet} from "@angular/router";
 import { ToastrModule } from 'ngx-toastr';
@@ -74,6 +75,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatTableModule,
     MatIconModule,
     MatSelectModule,
+    MatDatepickerModule,
     BrowserModule,
     ToastrModule.forRoot({
       timeOut: 3000,
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/authService';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  revealedPassword: boolean = false;
+  revealedPassword2: boolean = false;
+  registerForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telephone: ['', Validators.required],
+      dateBirth: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.passwordMatchValidator
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    const password = g.get('password');
+    const confirmPassword = g.get('confirmPassword');
+    return password === confirmPassword ? null : { 'passwordMismatch': true };
+  }
+
+  onSubmit(){
+    const name = this.registerForm.get('name')!.value;
+    const email = this.registerForm.get('email')!.value;
+    const telephone = this.registerForm.get('telephone')!.value;
+    const dateBirth = this.registerForm.get('dateBirth')!.value;
+    const password = this.registerForm.get('password')!.value;
+
+    console.log('Nome:', name);
+    console.log('Email:', email);
+    console.log('Telefone:', telephone);
+    console.log('Data nascimento:', dateBirth);
+    console.log('Senha:', password);
+  }
+
+  revealPassword(){
+    let inputPassword = document.getElementById('password');
+    if (this.revealedPassword) {
+      inputPassword!.setAttribute('type', 'password');
+      this.revealedPassword = false;
+    } else {
+      inputPassword!.setAttribute('type', 'text');
+      this.revealedPassword = true;
+    }
+  }
+
+  revealPassword2(){
+    let inputPassword = document.getElementById('confirmPassword');
+    if (this.revealedPassword2) {
+      inputPassword!.setAttribute('type', 'password');
+      this.revealedPassword2 = false;
+    } else {
+      inputPassword!.setAttribute('type', 'text');
+      this.revealedPassword2 = true;
+    }
+  }
 }
