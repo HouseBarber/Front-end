be39ab8a3cb9c4ab3cb4b7b07226c48f67c6f14d
joileJr <joilejunior2@gmail.com>
2023-07-31 20:08:12 -0300

    ajustando layout para enriquecimento cadastral

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Role } from 'src/app/models/role';
+import { AuthService } from 'src/app/services/authService';
+import { RolesService } from 'src/app/services/rolesService';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +13,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   panelOpenState = false;
+  profileForm!: FormGroup;
+  controlForm: { [key: string]: AbstractControl } = {};
+  roles: Role[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService,
+    private router: Router,
+    private rolesService: RolesService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeForms();
+    this.popularRoles();
+  }
+
+  initializeForms(): void {
+    this.profileForm = this.formBuilder.group({
+      username: [''],
+      name: [''],
+      cpf: [''],
+      cnpj: [''],
+      email: [''],
+      telephone: [''],
+      gender: [''],
+      dateBirth: [''],
+      description: [''],
+      roles: [''],
+      cep: [''],
+      city: [''],
+      state: [''],
+      neighborhood: [''],
+      street: [''],
+      number: [''],
+      complement: [''],
+    });
+    this.controlForm = this.profileForm.controls;
+  }
+
+  popularRoles(): void {
+    this.rolesService.getAllRoles().subscribe({
+      next: (response) => {
+        this.roles = response.object;
+      },
+      error: () => {
+        this.toastr.error("Erro ao buscar roles");
+      }
+    });
+  }
+
+
+  onSubmit() {
+
+  }
   
 }
