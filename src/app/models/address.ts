873dfde481668e15072b55aa7691c95f4daa6207
Joilejr <joilejunior2@gmail.com>
2023-09-13 20:52:30 -0300

    pau do address

--- src/app/models/address.ts
+++ src/app/models/address.ts
@@ -1,33 +1,30 @@
-import Auditable from "./auditable";
-
 export default class Address {
+    id?: number;
+    cep?: string;
+    city?: string;
+    state?: string;
+    neighborhood?: string;
+    street?: string;
+    number?: string;
+    complement?: string;
 
-  id?: number;
-  cep?: string;
-  city?: string;
-  state?: string;
-  neighborhood?: string;
-  street?: string;
-  number?: string;
-  complement?: string;
-
-  constructor(
-    id?: number,
-    cep?: string,
-    city?: string,
-    state?: string,
-    neighborhood?: string,
-    street?: string,
-    number?: string,
-    complement?: string,
-  ) {
-    this.id = id;
-    this.cep = cep;
-    this.city = city;
-    this.state = state;
-    this.neighborhood = neighborhood;
-    this.street = street;
-    this.number = number;
-    this.complement = complement;
-  }
+    constructor(
+        id?: number,
+        cep?: string,
+        city?: string,
+        state?: string,
+        neighborhood?: string,
+        street?: string,
+        number?: string,
+        complement?: string,
+    ) {
+        this.id = id;
+        this.cep = cep;
+        this.city = city;
+        this.state = state;
+        this.neighborhood = neighborhood;
+        this.street = street;
+        this.number = number;
+        this.complement = complement;
+    }
 }
